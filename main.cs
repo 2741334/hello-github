d3a992c76ef5f0957508d92ac62867a94e796170
Khalida Misraoui <misraoui.khalida@gmail.com>
2025-07-21 18:36:56 -0400

    Premier commit : projet Hello GitHub

---
+++ main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_github
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub! Je m'appelle Khalida Misraoui");
+        }
+    }
+}
